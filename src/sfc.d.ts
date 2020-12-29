2fafb98720f37fb5ac0024d872ec0d230fa75f29
manson <43368308+manson-2020@users.noreply.github.com>
2020-12-29 15:33:29 +0800

    Update sfc.d.ts

--- src/sfc.d.ts
+++ src/sfc.d.ts
@@ -1,4 +1,14 @@
+
+import { RequestInstance } from "./libs/request";
+import { Response } from "./common/api";
+
 declare module "*.vue" {
     import Vue from 'vue'
     export default Vue
+}
+
+declare module "vue/types/vue" {
+    interface Vue {
+        request: RequestInstance<Response>;
+    }
 }
