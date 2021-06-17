05af96151a8c84de24f5d9ceb2656d03229a0028
manson <43368308+manson-2020@users.noreply.github.com>
2021-06-17 10:14:06 +0800

    Update App.vue

--- App.vue
+++ App.vue
@@ -15,13 +15,9 @@
 
 		methods: {
 			async checkVersion() {
-				// #ifndef APP-PLUS
-				const {
-					onCheckForUpdate,
-					onUpdateReady,
-					onUpdateFailed,
-					applyUpdate,
-				} = uni.getUpdateManager();
+				// #ifndef MP
+				const { onCheckForUpdate, onUpdateReady, onUpdateFailed, applyUpdate } =
+					uni.getUpdateManager();
 
 				onCheckForUpdate(({ hasUpdate }) => {
 					/* 请求完新版本信息的回调 */
@@ -45,12 +41,77 @@
 					});
 				});
 				// #endif
+
+				// #ifdef APP-PLUS
+				const { checkVersion: checkVersionURL } = this.$config.path;
+				if (!checkVersionURL) return;
+				const { data, msg: title } = await this.$request.get(checkVersionURL, {
+					platform: uni.getSystemInfoSync().platform,
+					version: plus.runtime.version,
+				});
+
+				if (!data.upgradeUrl) return;
+
+				const [showModalFail, showModalRes]: any = await uni.showModal({
+					title,
+					content: data.description,
+					showCancel: Boolean(+data.forceUpdate),
+				});
+
+				if (showModalFail || !showModalRes.cancel) return;
+
+				if (uni.getSystemInfoSync().platform == "ios") {
+					plus.runtime.openURL(data.upgradeUrl);
+					return;
+				}
+
+				const showLoading = plus.nativeUI.showWaiting("开始下载...");
+
+				const downloadTask = uni.downloadFile({
+					url: data.upgradeUrl,
+					success: ({ tempFilePath }) => {
+						showLoading.setTitle("正在安装...");
+
+						plus.runtime.install(
+							<any>tempFilePath,
+							{},
+							(e) => {
+								plus.nativeUI.closeWaiting();
+								uni.showModal({
+									title: "更新完成",
+									content: "新版本需要重启APP",
+									confirmText: "立即重启",
+									showCancel: false,
+									success: plus.runtime.restart,
+								});
+							},
+							(error) => {
+								plus.nativeUI.closeWaiting();
+								uni.showToast({ title: "安装失败", icon: "none" });
+							}
+						);
+					},
+					fail() {
+						uni.showToast({ title: "下载失败", icon: "none" });
+					},
+				});
+
+				downloadTask.onProgressUpdate(
+					({ progress, totalBytesWritten, totalBytesExpectedToWrite }) =>
+						showLoading.setTitle(
+							`正在下载(${(totalBytesWritten / 1024 ** 2).toFixed(2)}MB/${(
+								totalBytesExpectedToWrite /
+								1024 ** 2
+							).toFixed(2)}MB)	${progress}%`
+						)
+				);
+				// #endif
 			},
 		},
 	});
 </script>
 
-<style lang="scss">
+<style>
 	page {
 		display: flex;
 		flex-direction: column;
@@ -61,159 +122,5 @@
 		font-weight: 500;
 		height: 100%;
 		background-color: #f7f7f7;
-	}
-
-	// 公共类
-	@for $flex from 1 through 5 {
-		.f#{$flex} {
-			flex: $flex;
-		}
-	}
-
-	@each $fd, $value in (col: column, row: row, rerow: row-reverse, recol: column-reverse) {
-		.fd-#{$fd} {
-			flex-direction: $value;
-		}
-	}
-
-	@each $align, $value in (start: flex-start, center: center, end: flex-end, baseline: baseline) {
-		.ai-#{$align} {
-			align-items: $value;
-		}
-		.as-#{$align} {
-			align-self: $value;
-		}
-		.ac-#{$align} {
-			align-content: $value;
-		}
-	}
-
-	@each $justify,
-		$value
-			in (
-				sa: space-around,
-				sb: space-between,
-				se: space-evenly,
-				center: center,
-				start: flex-start,
-				end: flex-end
-			)
-	{
-		.jc-#{$justify} {
-			justify-content: $value;
-		}
-		.js-#{$justify} {
-			justify-self: $value;
-		}
-		.ji-#{$justify} {
-			justify-items: $value;
-		}
-	}
-
-	@each $position in (fixed, relative, absolute, static) {
-		.#{$position} {
-			position: $position;
-		}
-	}
-
-	@each $text in (center, left, right, justify) {
-		.text-#{$text} {
-			text-align: $text;
-		}
-	}
-
-	@for $i from 0 through 50 {
-		@each $wrapper,
-			$box
-				in (
-					top: top,
-					bottom: bottom,
-					left: left,
-					right: right,
-					width: width,
-					height: height,
-					mt: margin-top,
-					mr: margin-right,
-					mb: margin-bottom,
-					ml: margin-left,
-					pt: padding-top,
-					pr: padding-right,
-					pb: padding-bottom,
-					pl: padding-left,
-					fs: font-size
-				)
-		{
-			.#{$wrapper}-#{$i * 2} {
-				#{$box}: #{$i * 2}rpx;
-			}
-		}
-
-		@if $i < 30 {
-			.br-#{$i * 2} {
-				border-radius: #{$i * 2}rpx;
-			}
-		}
-
-		@if $i <= 5 {
-			.lh-#{$i} {
-				line-height: $i;
-			}
-		} @else {
-			.lh-#{$i * 2} {
-				line-height: #{$i * 2}rpx;
-			}
-		}
-	}
-
-	@each $size in (auto, 20, 25, 30, 50, 70, 100) {
-		@if $size == 50 {
-			.br-circle {
-				border-radius: 50%;
-			}
-		}
-		@if $size == auto {
-			.width-#{$size} {
-				width: $size;
-			}
-			.height-#{$size} {
-				height: $size;
-			}
-		} @else {
-			.width-#{$size}vw {
-				width: #{$size}vw;
-			}
-			.width-#{$size}percent {
-				width: #{$size}#{"%"};
-			}
-
-			.height-#{$size}vh {
-				height: #{$size}vh;
-			}
-			.height-#{$size}percent {
-				height: #{$size}#{"%"};
-			}
-
-			.top-#{$size}percent {
-				top: #{$size}#{"%"};
-			}
-			.left-#{$size}percent {
-				left: #{$size}#{"%"};
-			}
-			.bottom-#{$size}percent {
-				bottom: #{$size}#{"%"};
-			}
-			.right-#{$size}percent {
-				right: #{$size}#{"%"};
-			}
-		}
-	}
-
-	@for $i from 1 to 10 {
-		.opacity-0#{$i} {
-			opacity: $i / 10;
-		}
-		.fw-#{$i * 100} {
-			font-weight: $i * 100;
-		}
 	}
 </style>
